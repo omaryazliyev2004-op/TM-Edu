4489a340fd606f61e07bd5168a7d44b46a2f3ffc
Omar <omaryazliyev2004@gmail.com>
2026-05-09 14:46:11 +0500

    Initial commit

---
+++ src/login.jsx
@@ -0,0 +1,74 @@
+import PersonIcon from "@mui/icons-material/Person";
+import LockIcon from '@mui/icons-material/Lock';
+export default function Login() {
+  return (
+    <div className="bg-[#F5F3FF]">
+      <section className="w-[90%] h-250 m-auto py-10 flex">
+        <div className="w-[50%]">
+          <img
+            src="/study.svg"
+            className=" h-[920px] bg-[#EEE8FF] rounded-l-[20px]"
+            alt=""
+          />
+        </div>
+        <div className="w-[50%] bg-white rounded-r-[20px] h-230 flex flex-col items-center pt-20 gap-5">
+          <h2 className="text-center w-90 text-[#4338CA] text-[16px] font-semibold">
+            MUHAMMAD AL-XORAZMIY NOMIDAGI TOSHKENT AXBOROT TEXNOLOGIYALARI
+            UNIVERSITETI
+          </h2>
+          <img src="/logo.png" className="w-48 h-40" alt="" />
+          <h2 className="font-medium text-[28px] tracking-[0.5px]">
+            LEARNING <span className="text-[#7C4DFF]">MANAGEMENT SYSTEM</span>
+          </h2>
+          <form className="flex flex-col gap-2 mt-3">
+            <h2>Login</h2>
+            <div className="relative mb-5">
+              <input
+                type="text"
+                placeholder="Loginni kiriting"
+                className="loginput outline-0 w-140 h-14 border-2 border-[#E9E5FF] rounded-xl pl-12 text-[14px] font-medium  "
+              />
+              <PersonIcon
+                sx={{
+                  position: "absolute",
+                  top: "16px",
+                  left: "12px",
+                  backgroundColor: "#EEE8FF",
+                  width: "26px",
+                  height: "26px",
+                  borderRadius: "6px",
+                  color: "#7C4DFF",
+                  padding: "4px",
+                }}
+              />
+            </div>
+             <h2>Parol</h2>
+            <div className="relative mb-5">
+              <input
+                type="password"
+                placeholder="Parolni kiriting"
+                className="loginput outline-0 w-140 h-14 border-2 border-[#E9E5FF] rounded-xl pl-12 text-[14px] font-medium"
+              />
+              <LockIcon
+                sx={{
+                  position: "absolute",
+                  top: "16px",
+                  left: "12px",
+                  backgroundColor: "#EEE8FF",
+                  width: "26px",
+                  height: "26px",
+                  borderRadius: "6px",
+                  color: "#7C4DFF",
+                  padding: "4px",
+                }}
+              />
+            </div>
+            <input type="submit"  className="outline-0 w-140 h-14 text-white bg-[#7C4DFF] rounded-xl  text-[14px] font-medium"/>
+          </form>
+          <hr className="w-140 h-0.5 text-[#00000011] bg-[#00000011] mt-22"/>
+          <h2 className="text-[#555555b2]">Copyright © 2021 of Tashkent University of Information Technologies</h2>
+        </div>
+      </section>
+    </div>
+  );
+}
